14ebc2c6284a55b75736a13d3cccd37495d001f3
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-15 16:59:27 -0700

    [menu] Add pizzaMenu object

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const pizzaMenu = {
+    hawaiianPizza: {
+        toppings: ['cheese', 'pineapple', 'ham'],
+        price: 2000,
+    },
+    pepperoniPizza: {
+        toppings: ['cheese', 'pepperoni', 'more pepperoni'],
+        price: 5000,
+    },
+    extraCheesePizza: {
+        toppings: ['cheese', 'sauce', 'more cheese'],
+        price: 1,
+    } ,
+    sausagePizza: {
+        toppings: ['cheese', 'sausage', 'sauce'],
+        price: 10000
+    } ,
+    veganPizza: {
+        toppings: ['broccoli', 'carrots', 'fake cheese'],
+        price: 'free'
+    }
+};
+console.log(pizzaMenu);
